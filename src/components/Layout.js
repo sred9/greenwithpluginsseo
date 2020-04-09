6801b1b9081db19656709a01c330598f3a32925d
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-04-10 00:26:05 +0530

    update

---
+++ src/components/Layout.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import _ from 'lodash';
+
+import {safePrefix} from '../utils';
+import Header from './Header';
+import Footer from './Footer';
+
+import { useStaticQuery, graphql } from 'gatsby';
+
+
+export default class Body extends React.Component {
+    render() {
+
+        return (
+            <React.Fragment>
+                <Helmet>
+                    <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
+                    <meta charSet="utf-8"/>
+                    <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.excerpt')}/>
+                           
+
+                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
+                    <meta name="google" content="notranslate" />
+
+                        
+                        
+                        
+                        
+<meta property="og:url"                content={_.get(this.props, 'pageContext.frontmatter.seourl')} />
+<meta property="og:type"               content="article" />
+<meta property="og:title"              content={_.get(this.props, 'pageContext.frontmatter.title')} />
+<meta property="og:description"        content={_.get(this.props, 'pageContext.frontmatter.excerpt')} />
+<meta property="og:image"              content={_.get(this.props, 'pageContext.frontmatter.image')} />
+                        
+  <meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@flickr" />
+<meta name="twitter:title" content={_.get(this.props, 'pageContext.frontmatter.title')} />
+<meta name="twitter:description" content={_.get(this.props, 'pageContext.frontmatter.excerpt')} />
+<meta name="twitter:image" content={_.get(this.props, 'pageContext.frontmatter.image')} />                      
+                        
+                        
+                        
+                        
+                        
+                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
+                </Helmet>
+                <div id="page" className="site">
+                  <Header {...this.props} />
+                  <main id="content" className="site-content">
+                    {this.props.children}
+                  </main>
+                  <Footer {...this.props} />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
